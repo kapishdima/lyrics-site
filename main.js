fb7c5127e8e2e47e486962ab3a84849fd97ed533
DimaDevelopment <dima.development@fakel.com.ua>
2023-07-04 22:54:29 +0300

    replace bg color animation. add illustrations. fixed font-sizes

--- main.js
+++ main.js
@@ -3,6 +3,27 @@ import Swiper, { Mousewheel } from 'swiper';
 import 'swiper/css';
 import './style.css';
 
+const colors = [
+  'rgba(46, 204, 113, 1.0)',
+  'rgba(230, 126, 34, 1.0)',
+  'rgba(26, 188, 156, 1.0)',
+  'rgba(46, 204, 113, 1.0)',
+  'rgba(230, 126, 34, 1.0)',
+  'rgba(26, 188, 156, 1.0)',
+  'rgba(46, 204, 113, 1.0)',
+  'rgba(230, 126, 34, 1.0)',
+  'rgba(26, 188, 156, 1.0)',
+  'rgba(46, 204, 113, 1.0)',
+  'rgba(230, 126, 34, 1.0)',
+  'rgba(26, 188, 156, 1.0)',
+  'rgba(46, 204, 113, 1.0)',
+  'rgba(230, 126, 34, 1.0)',
+  'rgba(26, 188, 156, 1.0)',
+  'rgba(46, 204, 113, 1.0)',
+  'rgba(230, 126, 34, 1.0)',
+  'rgba(26, 188, 156, 1.0)',
+];
+
 const swiper = new Swiper('.mySwiper', {
   modules: [Mousewheel],
   direction: 'vertical',
@@ -11,4 +32,15 @@ const swiper = new Swiper('.mySwiper', {
   mousewheel: true,
   speed: 1000,
   loop: true,
+});
+
+const color = colors[0];
+
+document.body.style.backgroundColor = color;
+
+swiper.on('scroll', (swiper, event) => {
+  const active = swiper.realIndex;
+  const color = colors[active];
+
+  document.body.style.backgroundColor = color;
 });
